7cca94c845fe9d3d0e385a904a67d3256cf42e9c
Michael <michael.pippin.0704@gmail.com>
2016-04-25 15:32:47 +0800

    2

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,4 +1,5 @@
 Hello Yose
+<a id="ping-challenge-link" href="/ping">ping</a>
 <a id="repository-link" href="https://github.com/ChupaChupsScrumTeam/ChupaChupsYosee">YoseTheGame</a>
 
 <a id="contact-me-link" href="www.google.com">Contact Me</a>
