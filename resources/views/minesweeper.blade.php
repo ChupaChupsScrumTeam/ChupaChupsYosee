f29aae50c0c5458237f45c842f88bb2c5d1a02a4
githubofjon <abacajpm@gmail.com>
2016-04-27 15:48:49 +0800

    On branch master modified:   resources/views/minesweeper.blade.php
    load data minesweeper

--- resources/views/minesweeper.blade.php
+++ resources/views/minesweeper.blade.php
@@ -1,5 +1,22 @@
 <p id="title">Minesweeper</p>
 
+    <div id="board">
 @foreach( $ids as $id )
     <div id="{{ $id }}"></div>
 @endforeach
+    </div>
+
+<script type="text/javascript">
+    function load() {
+        var data = document.grid;
+        var cell = 0;
+        for(var i = 0; i < data.length; i++) {
+            for(var j = 0; j < data[i].length; i++) {
+                if(data[i][j] === 'bomb') {
+                    document.getElementById('board').children[cell].setAttribute('class', 'lost');
+                }
+                cell = cell + 1;
+            }
+        }
+    }
+</script>
